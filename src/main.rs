48e712eac7437e565ae8d874a78fa84c5ff706a4
Colin <colin.vandervoort.42@gmail.com>
2023-07-22 22:01:19 -0700

    first version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io::{self};
+
 fn main() {
-    println!("Hello, world!");
+    let line_sep = "\n";
+    let lines = io::stdin().lines();
+    let mut string_vec: Vec<String> = Vec::new();
+    for line in lines {
+        match line {
+            Ok(line_string) => {
+                string_vec.extend(line_string.split(line_sep).map(|token| String::from(token)));
+            },
+            Err(error) => println!("Error: {}", error),
+        }
+    }
+
+    string_vec.sort();
+
+    for string in string_vec.iter() {
+        println!("Token: {}", string);
+    }
 }
