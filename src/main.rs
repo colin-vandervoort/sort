6dded43b83e42cceecfc1a216cc2dd94b73df13a
Colin <colin.vandervoort.42@gmail.com>
2023-08-14 00:00:54 -0700

    fix: allow stdin and other files to be sorted together

--- src/main.rs
+++ src/main.rs
@@ -11,25 +11,31 @@ mod parse;
 static FLAG_FILE: &str = "file";
 static FLAG_REVERSE: &str = "reverse";
 
+static STDIN_KEYWORD: &str = "-";
+
 struct SortSettings {
     ascend: bool,
+    sort_stdin: bool,
 }
 
 fn main() {
     let cli_matches = cli().get_matches();
 
-    let mut file_paths: Option<Vec<&String>> = None;
-    if let Some(file_arg_matches) = cli_matches.get_many::<String>(FLAG_FILE) {
-        file_paths = Some(file_arg_matches.collect());
-    }
+    let mut file_paths = match cli_matches.get_many::<String>(FLAG_FILE) {
+        Some(file_arg_matches) => file_arg_matches.collect(),
+        None => Vec::new(),
+    };
+
+    let sort_stdin = file_paths.iter().any(|&s| s == STDIN_KEYWORD) || (file_paths.len() == 0);
+    // remove '-' if it is present in the vec
+    file_paths.retain(|&p| p != STDIN_KEYWORD);
 
     let settings = SortSettings {
         ascend: !cli_matches.get_flag(FLAG_REVERSE),
+        sort_stdin,
     };
-    match file_paths {
-        Some(paths) => sort_files(&settings, paths),
-        None => sort_stdin(&settings),
-    }
+
+    sort_all(&settings, file_paths);
 }
 
 fn cli() -> Command {
@@ -43,11 +49,10 @@ fn cli() -> Command {
         .arg(Arg::new(FLAG_FILE).num_args(0..))
 }
 
-fn sort_files(settings: &SortSettings, file_paths: Vec<&String>) {
+fn sort_all(settings: &SortSettings, file_paths: Vec<&String>) {
     let mut line_accumulator: Vec<String> = Vec::new();
 
-    let paths = file_paths.iter().map(|path_str| path::Path::new(path_str));
-    for path in paths {
+    for path in file_paths.iter().map(|path_str| path::Path::new(path_str)) {
         match path.try_exists() {
             Ok(exists) if (exists && path.is_dir()) => {
                 eprintln!("sort: Is a directory");
@@ -72,21 +77,14 @@ fn sort_files(settings: &SortSettings, file_paths: Vec<&String>) {
         }
     }
 
-    line_accumulator.sort_by(|a, b| line_order(settings, a, b));
-
-    for string in line_accumulator.iter() {
-        println!("{}", string);
-    }
-}
-
-fn sort_stdin(settings: &SortSettings) {
-    let mut line_accumulator: Vec<String> = Vec::new();
-    for input_line in io::stdin().lines() {
-        match input_line {
-            Ok(line_string) => parse::tokenize_line(&mut line_accumulator, &line_string),
-            Err(error) => {
-                eprintln!("Error: {}", error);
-                std::process::exit(1);
+    if settings.sort_stdin {
+        for input_line in io::stdin().lines() {
+            match input_line {
+                Ok(line_string) => parse::tokenize_line(&mut line_accumulator, &line_string),
+                Err(error) => {
+                    eprintln!("Error: {}", error);
+                    std::process::exit(1);
+                }
             }
         }
     }
