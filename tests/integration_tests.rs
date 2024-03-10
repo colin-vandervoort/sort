5d795377965d3f6d6c546b872ee820714732d0e5
Colin <colin.vandervoort.42@gmail.com>
2024-03-10 12:23:23 -0700

    refactor: DRY test code

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,89 +1,33 @@
-use std::{io::Write, process::Command, process::Stdio};
+use std::collections::HashMap;
 
 mod util;
 
 #[test]
 fn test_sort_single_file() {
-    let mut cmd_expect: std::process::Command = Command::new("sort");
-    cmd_expect.stdin(Stdio::piped());
-
-    let mut cmd_actual: std::process::Command = Command::new("cargo");
-    cmd_actual
-        .arg("run")
-        .arg("--quiet")
-        .arg("--")
-        .stdin(Stdio::piped());
-
-    let common_args = vec!["tests/data/breakfast.txt"];
-    cmd_expect.args(common_args.clone());
-    cmd_actual.args(common_args.clone());
-
-    println!("cmd_golden: {:?}", cmd_expect);
-    println!("cmd_under_test: {:?}", cmd_actual);
-
-    let output_expect = cmd_expect
-        .output()
-        .expect("failed to execute process for golden command");
-    let output_actual = cmd_actual
-        .output()
-        .expect("failed to execute process for command under test");
-
-    util::cmp_cmd_output(output_expect, output_actual);
+    let test_params = util::TestParams {
+        env_vars: HashMap::new(),
+        stdin: None,
+        args: &["tests/data/breakfast.txt"],
+    };
+    util::cmp_actual_expect(test_params);
 }
 
 #[test]
 fn test_sort_stdin() {
-    let mut cmd_expect: std::process::Command = Command::new("sort");
-    cmd_expect
-        .stdin(Stdio::piped())
-        .stdout(Stdio::piped())
-        .stderr(Stdio::piped());
+    let test_params = util::TestParams {
+        env_vars: HashMap::new(),
+        stdin: Some("bananas\napples\n"),
+        args: &[],
+    };
+    util::cmp_actual_expect(test_params);
+}
 
-    let mut cmd_actual: std::process::Command = Command::new("cargo");
-    cmd_actual
-        .arg("run")
-        .arg("--quiet")
-        .arg("--")
-        .stdin(Stdio::piped())
-        .stdout(Stdio::piped())
-        .stderr(Stdio::piped());
-
-    let common_args = vec!["-"];
-    cmd_expect.args(common_args.clone());
-    cmd_actual.args(common_args.clone());
-
-    println!("cmd_golden: {:?}", cmd_expect);
-    println!("cmd_under_test: {:?}", cmd_actual);
-
-    let mut child_expect = cmd_expect
-        .spawn()
-        .expect("failed to execute process for golden command");
-    let mut child_actual = cmd_actual
-        .spawn()
-        .expect("failed to execute process for command under test");
-
-    let mut stdin_expect = child_expect.stdin.take().expect("failed to take stdin");
-    let handle_expect = std::thread::spawn(move || {
-        stdin_expect
-            .write_all("bananas\napples\n".as_bytes())
-            .expect("failed to write to stdin");
-    });
-    let mut stdin_actual = child_actual.stdin.take().expect("failed to take stdin");
-    let handle_actual = std::thread::spawn(move || {
-        stdin_actual
-            .write_all("bananas\napples\n".as_bytes())
-            .expect("failed to write to stdin");
-    });
-
-    handle_expect.join().expect("failed to join thread");
-    handle_actual.join().expect("failed to join thread");
-
-    let output_expect = child_expect
-        .wait_with_output()
-        .expect("failed to read stdout");
-    let output_actual = child_actual
-        .wait_with_output()
-        .expect("failed to read stdout");
-
-    util::cmp_cmd_output(output_expect, output_actual);
+#[test]
+fn test_check_file() {
+    let test_params = util::TestParams {
+        env_vars: HashMap::new(),
+        stdin: None,
+        args: &["-c", "tests/data/lunch.txt"],
+    };
+    util::cmp_actual_expect(test_params);
 }
--- tests/util.rs
+++ tests/util.rs
@@ -1,24 +1,70 @@
-// #[macro_export]
-// macro_rules! cmp_cmd {
-//     ($test_name:ident, $cmd_golden:expr, $cmd_under_test:expr) => {
-//         #[test]
-//         fn $test_name() {
-//             let mut cmd_golden: std::process::Command = $cmd_golden;
-//             let mut cmd_under_test: std::process::Command = $cmd_under_test;
+use std::collections::HashMap;
+use std::io::Write;
+use std::process::{Command, Output, Stdio};
 
-//             let output_expect = cmd_golden
-//                 .output()
-//                 .expect("failed to execute process for golden command");
-//             let output_actual = cmd_under_test
-//                 .output()
-//                 .expect("failed to execute process for command under test");
+pub struct TestParams<'a> {
+    pub env_vars: HashMap<String, String>,
+    pub stdin: Option<&'static str>,
+    pub args: &'a [&'a str],
+}
 
-//             assert_eq!(output_actual)
-//         }
-//     };
-// }
+struct CommandWrapper<'a> {
+    alias: &'a str,
+    cmd: Command,
+}
 
-use std::process::Output;
+pub fn cmp_actual_expect(params: TestParams) {
+    let mut actual = CommandWrapper {
+        alias: "actual",
+        cmd: Command::new("cargo"),
+    };
+    actual.cmd.arg("run").arg("--quiet").arg("--");
+
+    let mut expect = CommandWrapper {
+        alias: "expect",
+        cmd: Command::new("sort"),
+    };
+
+    let [child_actual, child_expect] = [&mut actual, &mut expect].map(|cmd_wrapper| {
+        cmd_wrapper.cmd.args(params.args);
+        cmd_wrapper.cmd.envs(params.env_vars.clone());
+        cmd_wrapper
+            .cmd
+            .stdin(Stdio::piped())
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped());
+
+        println!("cmd_{}: {:?}", cmd_wrapper.alias, cmd_wrapper.cmd);
+
+        let mut child = cmd_wrapper
+            .cmd
+            .spawn()
+            .expect(format!("failed to execute process for cmd_{0}", cmd_wrapper.alias).as_str());
+
+        let mut child_stdin = child.stdin.take().expect("failed to take stdin");
+        let handle = std::thread::spawn(move || {
+            if let Some(stdin_str) = params.stdin.clone() {
+                child_stdin
+                    .write_all(stdin_str.as_bytes())
+                    .expect("failed to write to stdin");
+            }
+        });
+        handle
+            .join()
+            .expect(format!("failed to join thread for cmd_{0}", cmd_wrapper.alias).as_str());
+
+        child
+    });
+
+    let output_actual = child_actual
+        .wait_with_output()
+        .expect("failed to read stdout");
+    let output_expect = child_expect
+        .wait_with_output()
+        .expect("failed to read stdout");
+
+    cmp_cmd_output(output_expect, output_actual);
+}
 
 pub fn cmp_cmd_output(output_expect: Output, output_actual: Output) {
     let expect_stdout_string = String::from_utf8(output_expect.stdout).expect("not valid utf8");
