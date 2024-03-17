d5a9eaca236ed71e9aeb91bc7281a131619ce428
Colin <colin.vandervoort.42@gmail.com>
2024-03-16 21:42:17 -0700

    refactor: rename crate and improve tests organization

--- tests/cmp_sort_from_path.rs
+++ tests/cmp_sort_from_path.rs
@@ -1,87 +1,88 @@
-use std::collections::HashMap;
-
 mod util;
+
+use std::collections::HashMap;
+use crate::util::{TestParams, cmp_actual_expect};
 
 #[test]
 fn test_sort_single_file() {
-    let test_params = util::TestParams {
+    let test_params = TestParams {
         env_vars: HashMap::new(),
         stdin: None,
         args: &["tests/data/breakfast.txt"],
     };
-    util::cmp_actual_expect(test_params);
+    cmp_actual_expect(test_params);
 }
 
 #[test]
 fn test_sort_stdin() {
-    let test_params = util::TestParams {
+    let test_params = TestParams {
         env_vars: HashMap::new(),
         stdin: Some("bananas\napples\n"),
         args: &[],
     };
-    util::cmp_actual_expect(test_params);
+    cmp_actual_expect(test_params);
 }
 
 #[test]
 fn test_check_file() {
-    let test_params = util::TestParams {
+    let test_params = TestParams {
         env_vars: HashMap::new(),
         stdin: None,
         args: &["-c", "tests/data/lunch.txt"],
     };
-    util::cmp_actual_expect(test_params);
+    cmp_actual_expect(test_params);
 }
 
 #[test]
 fn test_check_file_unique() {
-    let test_params = util::TestParams {
+    let test_params = TestParams {
         env_vars: HashMap::new(),
         stdin: None,
         args: &["-c", "-u", "tests/data/lunch-sorted-non-uniq.txt"],
     };
-    util::cmp_actual_expect(test_params);
+    cmp_actual_expect(test_params);
 }
 
 #[test]
 fn test_sort_file_unique() {
-    let test_params = util::TestParams {
+    let test_params = TestParams {
         env_vars: HashMap::new(),
         stdin: None,
         args: &["-u", "tests/data/lunch-sorted-non-uniq.txt"],
     };
-    util::cmp_actual_expect(test_params);
+    cmp_actual_expect(test_params);
 }
 
 #[test]
 fn test_sort_numeric_en() {
-    let test_params = util::TestParams {
+    let test_params = TestParams {
         env_vars: HashMap::from([
             ("LANG", "en_EN")
         ]),
         stdin: None,
         args: &["-n", "tests/data/numeric.txt"],
     };
-    util::cmp_actual_expect(test_params);
+    cmp_actual_expect(test_params);
 }
 
 #[test]
 fn test_sort_numeric_de() {
-    let test_params = util::TestParams {
+    let test_params = TestParams {
         env_vars: HashMap::from([
             ("LANG", "De_DE")
         ]),
         stdin: None,
         args: &["-n", "tests/data/numeric.txt"],
     };
-    util::cmp_actual_expect(test_params);
+    cmp_actual_expect(test_params);
 }
 
 #[test]
 fn test_zero_terminated() {
-    let test_params = util::TestParams {
+    let test_params = TestParams {
         env_vars: HashMap::new(),
         stdin: None,
         args: &["-z", "tests/data/nul-dip"],
     };
-    util::cmp_actual_expect(test_params);
+    cmp_actual_expect(test_params);
 }
